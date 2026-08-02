93c22593f714642ed5bd09131b257184664d8744
Jan Ehlen <jan@example.com>
2026-08-02 12:58:01 +0200

    Initial Commit

---
+++ DocPilot/DocPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocPilotApp.swift
+//  DocPilot
+//
+//  Created by Jan Ehlen on 02.08.26.
+//
+
+import SwiftUI
+
+@main
+struct DocPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
